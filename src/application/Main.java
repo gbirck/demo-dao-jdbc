079e117367a82058111f36ddd5d3c9143c7efa85
Gabriel Birck <birckzin@gmail.com>
2024-10-25 15:29:35 -0300

    Adicionado DepartmentDAO e SellerDAO

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         Department department= new Department(1, "Books");
         Seller seller = new Seller(21, new Date(), "bob", "bob@gmail.com", 3000.0, department);
-        
+
         System.out.println(seller.toString());
     }
 }
