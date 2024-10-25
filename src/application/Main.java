e70493658cb7430a780546ed70411e35c4d470a7
Gabriel Birck <birckzin@gmail.com>
2024-10-25 15:54:35 -0300

    Melhorado o codigo do SellerDao

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,8 @@ public class Main {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("=== SELLER FIND BY ID ===");
         Seller seller = sellerDao.findById(1);
 
         System.out.println(seller.toString());
